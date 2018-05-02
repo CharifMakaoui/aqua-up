138e3574b0034e85e8f1d7fa04cea8eb7eeb83d8
MrCharif <mrcharifmakaoui@gmail.com>
2018-05-02 11:05:23 +0100

    full switch to youtube-dl nodejs now no ned to python :D

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
     return res.send('coming soon!');
 });
 
-app.get('/url/:token', (req, res) =>{
+app.get('/init/:token', (req, res) =>{
 
     let token = JSON.parse(laravel.decrypt(req.params.token));
 
@@ -22,78 +22,37 @@ app.get('/url/:token', (req, res) =>{
         let options = [];
 
         youtubedl.getInfo(url, options, {}, function(err, info) {
-            if (err) throw err;
+            if (err) {
+                return res.json({
+                    status : 'error',
+                    message : err
+                });
+            }
 
-            console.log('id:', info.id);
-            console.log('title:', info.title);
-            console.log('url:', info.url);
-            console.log('thumbnail:', info.thumbnail);
-            console.log('description:', info.description);
-            console.log('filename:', info._filename);
-            console.log('format id:', info.format_id);
+            let createAt = new Date();
 
-            res.json({
-                status : "done",
-                data : info
+            let data = {
+                title : info.title,
+                url   : info.url,
+                server   : token.server,
+                createAt : createAt.getTime()
+            };
+
+
+            return res.json({
+                status: 'ticket',
+                response : data,
             })
         });
     }
     else {
-        res.json({
+        return res.json({
             status : 'error',
             error : 'token',
             message : 'invalid parameter for this token check your encryption key !'
         })
     }
 
-});
-
-app.get('/init/:token', (req, res) => {
-
-    let token = JSON.parse(laravel.decrypt(req.params.token));
-
-    if(token){
-        request('http://localhost:1337/api/info?url=' + token.url + '&flatten=False', function (error, response, body) {
-
-            // create new token
-            if(body){
-                body = JSON.parse(body);
-                let createAt = new Date();
-
-                if(body.hasOwnProperty('error')){
-                    return res.json({
-                        status : 'error',
-                        message : body.error
-                    });
-                }
-
-                let data = {
-                    title : body.info.title,
-                    url   : body.info.url,
-                    server   : token.server,
-                    createAt : createAt.getTime()
-                };
-
-                return res.json({
-                    status: 'ticket',
-                    response : data,
-                })
-            }
-
-        });
-    }
-    else {
-        res.json({
-            status : 'error',
-            error : 'token',
-            message : 'invalid parameter for this token check your encryption key !'
-        })
-    }
-
-
-    /*let video = request('http://localhost:1337/api/play?url=' + token.url);
-    req.pipe(video);
-    video.pipe(res);*/
 });
 
 app.get('/stream/:name', (req, res) => {
@@ -103,7 +62,6 @@ app.get('/stream/:name', (req, res) => {
     let video = request(urlToPaly);
     req.pipe(video);
     video.pipe(res);
-
 });
 
 
